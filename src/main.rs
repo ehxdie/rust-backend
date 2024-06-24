0b9c55d0fa12a64752a4fbb7653647556ef34d9b
Edidiong <ephraimedy@gmail.com>
2024-06-24 17:26:57 +0100

    Created files and setup routes

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,24 @@
 use chrono::prelude::*;
+use serde::{Serialize, Deserialize};
+use db::DB;
+use std::convert::Infallible;
+
+// The webframework used to create the server and routing
+use warp::{Filter, Rejection};
+
+// Importing modules
+mod db;
+mod error;
+mod handler;
+
+// General Result type
+type Result<T> = std::result::Result<T, error::Error>;
+// Result type for webresult 
+type WebResult<T> = std::result::Result<T, Rejection>;
+
+
+// For changing data formats, and the debug trait 
+#[derive(Debug, Serialize, Deserialize)]
 pub struct workout{
     pub title: String,
     pub load: String,
@@ -6,6 +26,41 @@ pub struct workout{
     pub added_at: DateTime<Utc>
 }
 
-fn main() {
-    println!("Hello, world!");
+#[tokio::main]
+async fn main() -> Result<String> {
+    let db = DB::init().await?;
+    let workout = warp::path("api/workouts");
+
+    let workout_routes = workout
+        .and(warp::post())
+        .and(warp::body::json())
+        .and(with_db(db.clone()))
+        .and_then(handler::create_workout)
+        .or(workout
+            .and(warp::put())
+            .and(warp::path::param())
+            .and(warp::body::json())
+            .and(with_db(db.clone()))
+            .and_then(handler::update_workout))
+        .or(workout
+            .and(warp::delete())
+            .and(warp::path::param())
+            .and(with_db(db.clone()))
+            .and_then(handler::delete_workout))
+        .or(workout
+            .and(warp::get())
+            .and(with_db(db.clone()))
+            .and_then(handler::get_workout));
+
+   
+    let routes = workout_routes.recover(error::handle_rejection);
+
+    println!("Started on port 8080");
+    warp::serve(routes).run(([0,0,0,0], 8080)).await;
+    Ok(());
+}
+
+// No idea what this does tbh
+fn with_db(db: DB) -> impl Filter<Extract = (DB,), Error = Infallible> + Clone {
+    warp::any().map(move || db.clone())
 }
